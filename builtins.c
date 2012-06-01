4f5f75fb975e715fff70dd31f5ccaaf3e065cecb
Christian Stigen Larsen <csl@sublevel3.org>
2012-06-01 07:27:15 +0200

    chdir without arg -> home

--- builtins.c
+++ builtins.c
@@ -24,8 +24,9 @@ int isbuiltin(const char* s)
 void exec_builtin(const char* cmd, char* const* argv)
 {
   if ( !strcmp("cd", cmd) ) {
-    chdir(argv[1]);
-    fprintf(stderr, "chdir to '%s'\n", argv[1]);
+    const char *path = !argv[1]? getenv("HOME") : argv[1];
+    fprintf(stderr, "chdir to '%s'\n", path);
+    chdir(path);
   } else if ( !strcmp("help", cmd) ) {
     fprintf(stderr, "skall is a minimal, experimental unix shell\n\n");
     fprintf(stderr, "builtin commands:\n");
